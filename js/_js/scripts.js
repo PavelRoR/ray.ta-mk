6bf16d85355db4434e3ae0d76f27515d960b793b
pavelp21 <pavelvp2182@yandex.ru>
2019-10-24 21:16:33 +0500

    Pages are adepted for IE

--- js/_js/scripts.js
+++ js/_js/scripts.js
@@ -3,6 +3,16 @@
 //@prepros-prepend lazy.min.js
 //@prepros-prepend jquery.fancybox.min.js
 
+var isIE = false || !!document.documentMode;
+
+if (isIE) {
+    var head = document.getElementsByTagName("head")[0];
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "css/ie.min.css";
+    head.appendChild(link);
+}
+
 $(document).ready(function () {
     $(function () {
         var check = $('.check', this),
